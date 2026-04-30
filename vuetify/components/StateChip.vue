9f296a0c7e9ce12c675636c2e6d51e95ce6d1909
natholdallas <natholdallas@gmail.com>
2026-04-30 16:16:54 +0800

    commit

---
+++ vuetify/components/StateChip.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{
+  state: boolean
+}>()
+</script>
+
+<template>
+  <VChip :color="state ? 'success' : 'disabled'" :text="state ? $t('enabled') : $t('disabled')" />
+</template>
