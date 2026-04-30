072129b903dab2fc15b68d0b361ef4d7848171d3
natholdallas <natholdallas@gmail.com>
2026-04-30 14:23:15 +0800

    commit

--- pinia/index.ts
+++ pinia/index.ts
@@ -1,4 +1,4 @@
-import { addImportsDir, createResolver, defineNuxtModule } from '@nuxt/kit'
+import { defineNuxtModule } from '@nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
@@ -10,9 +10,9 @@ export default defineNuxtModule({
     'pinia-plugin-persistedstate/nuxt': {},
   },
 
-  async setup() {
-    const resolver = createResolver(import.meta.url)
-
-    addImportsDir(resolver.resolve('./stores'))
-  },
+  // async setup() {
+  //   const resolver = createResolver(import.meta.url)
+  //
+  //   addImportsDir(resolver.resolve('./stores'))
+  // },
 })
