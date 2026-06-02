c8c6024358e98435ef9356a41ab6f333334db386
natholdallas <natholdallas@gmail.com>
2026-06-02 14:14:01 +0800

    commit

---
+++ shadcn/components/modules/Img.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps<{
+  alt?: string
+  crossorigin?: 'anonymous' | 'use-credentials' | ''
+  decoding?: 'async' | 'auto' | 'sync'
+  fetchpriority?: 'high' | 'low'
+  height?: number | string
+  loading?: 'eager' | 'lazy'
+  referrerpolicy?:
+    | 'no-referrer'
+    | 'no-referrer-when-downgrade'
+    | 'origin'
+    | 'origin-when-cross-origin'
+    | 'same-origin'
+    | 'strict-origin'
+    | 'strict-origin-when-cross-origin'
+    | 'unsafe-url'
+  sizes?: string
+  src: string
+  srcset?: string
+  usemap?: string
+  width?: number | string
+}>()
+
+const flagLoaded = ref(false)
+</script>
+
+<template>
+  <img
+    @load="flagLoaded = true"
+    @error="flagLoaded = false"
+    :alt="alt"
+    :crossorigin="crossorigin"
+    :decoding="decoding"
+    :fetchpriority="fetchpriority"
+    :height="height"
+    :loading="loading"
+    :referrerpolicy="referrerpolicy"
+    :sizes="sizes"
+    :src="src"
+    :srcset="srcset"
+    :usemap="usemap"
+    :width="width"
+  />
+</template>
