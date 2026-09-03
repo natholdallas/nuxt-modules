1218cd123393f3bd87c03393ca7c9de4aed0d6b9
natholdallas <natholdallas@gmail.com>
2026-09-03 19:59:08 +0800

    upd

--- vuetify/components/TopTableBar.vue
+++ vuetify/components/TopTableBar.vue
@@ -8,16 +8,23 @@ const showCreate = defineModel<boolean>()
 </script>
 
 <template>
-  <VToolbar density="comfortable" flat>
-    <VToolbarTitle v-if="text" :text="text" />
-    <slot v-else />
-    <template #append>
+  <div class="top-table-bar overflow-x-auto">
+    <div class="flex min-w-full w-max items-center gap-2 px-3 py-2">
+      <span v-if="text" class="shrink-0 whitespace-nowrap font-medium">{{ text }}</span>
+      <slot v-else />
       <VBtn
         v-if="!noAppend"
         prepend-icon="mdi-plus"
         :text="$t('create')"
+        class="shrink-0"
         @click="showCreate = true"
       />
-    </template>
-  </VToolbar>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.top-table-bar {
+  background: rgb(var(--v-theme-surface-light));
+}
+</style>
